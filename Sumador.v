8be025248144498677125ceff95330c51a2bc027
Umana004 <159400472+Umana004@users.noreply.github.com>
2025-06-04 14:25:36 -0600

    Modulo de sumador

--- Sumador.v
+++ Sumador.v
@@ -1,1 +1,11 @@
-
+// Módulo sumador completo de un bit
+module SumadorCompleto(
+    input A,
+    input B,
+    input Cin,
+    output S,
+    output Cout
+);
+    assign S = A ^ B ^ Cin; // Calcula la suma
+    assign Cout = (A & B) | (B & Cin) | (A & Cin); // Calcula el acarreo de salida
+endmodule
